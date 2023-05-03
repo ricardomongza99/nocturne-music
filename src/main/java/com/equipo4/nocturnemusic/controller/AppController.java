6bdd473981b3e790c475540e18ce624123b200f8
Ricardo Montemayor <ricardomongza@gmail.com>
2023-05-02 21:15:13 -0600

    Change RequestMapping to GetMapping

--- src/main/java/com/equipo4/nocturnemusic/controller/AppController.java
+++ src/main/java/com/equipo4/nocturnemusic/controller/AppController.java
@@ -1,12 +1,12 @@
 package com.equipo4.nocturnemusic.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String showHomePage() {
         return "product/products";
     }
