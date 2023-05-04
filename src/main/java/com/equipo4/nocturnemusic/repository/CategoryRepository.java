d2cfb8b9b358744a6f7299dd23caa95acfaa298a
Ricardo Montemayor <ricardomongza@gmail.com>
2023-05-04 02:16:20 -0600

    Add @Repository decorator

--- src/main/java/com/equipo4/nocturnemusic/repository/CategoryRepository.java
+++ src/main/java/com/equipo4/nocturnemusic/repository/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.equipo4.nocturnemusic.repository;
 
 import com.equipo4.nocturnemusic.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
