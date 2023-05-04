7f87c5bdbd0c765b5dc98d05b412e1447a6560ab
Ricardo Montemayor <ricardomongza@gmail.com>
2023-05-04 02:07:51 -0600

    Add ProductController and templates

--- src/main/java/com/equipo4/nocturnemusic/controller/ProductController.java
+++ src/main/java/com/equipo4/nocturnemusic/controller/ProductController.java
@@ -1,2 +1,58 @@
-package com.equipo4.nocturnemusic.controller;public class ProductController {
+package com.equipo4.nocturnemusic.controller;
+
+import com.equipo4.nocturnemusic.model.Product;
+import com.equipo4.nocturnemusic.service.CategoryService;
+import com.equipo4.nocturnemusic.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public String listProducts(Model model) {
+        model.addAttribute("products", productService.findAll());
+        return "product/list";
+    }
+
+    @GetMapping("/add")
+    public String showAddProductForm(Model model) {
+        model.addAttribute("product", new Product());
+        model.addAttribute("categories", categoryService.findAll());
+        return "product/form";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditProductForm(@PathVariable("id") Long id, Model model) {
+        Optional<Product> product = productService.findById(id);
+        model.addAttribute("categories", categoryService.findAll());
+        product.ifPresent(p -> model.addAttribute("product", p));
+        return product.isPresent() ? "product/form" : "redirect:/products";
+    }
+
+    @PostMapping
+    public String saveProduct(Product product) {
+        productService.save(product);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable("id") Long id) {
+        productService.deleteById(id);
+        return "redirect:/products";
+    }
 }
