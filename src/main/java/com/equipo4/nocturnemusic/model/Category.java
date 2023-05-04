de489c22229b481ac073b3a222bf23be559feb64
Ricardo Montemayor <ricardomongza@gmail.com>
2023-05-03 20:15:10 -0600

    Add models

--- src/main/java/com/equipo4/nocturnemusic/model/Category.java
+++ src/main/java/com/equipo4/nocturnemusic/model/Category.java
@@ -1,2 +1,44 @@
-package com.equipo4.nocturnemusic.model;public class Category {
+package com.equipo4.nocturnemusic.model;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private Set<Product> products;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
 }
--- src/main/java/com/equipo4/nocturnemusic/model/Product.java
+++ src/main/java/com/equipo4/nocturnemusic/model/Product.java
@@ -1,2 +1,71 @@
-package com.equipo4.nocturnemusic.model;public class Product {
+package com.equipo4.nocturnemusic.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String imageUrl;
+
+    @ManyToOne
+    private Category category;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
