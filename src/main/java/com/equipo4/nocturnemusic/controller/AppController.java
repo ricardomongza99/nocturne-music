4e6f5bb10158ad75f605895096cf20e27667d7cd
dmacias77 <dmacias77@outlook.com>
2023-06-07 13:03:54 -0600

    Index, home & static resources

--- src/main/java/com/equipo4/nocturnemusic/controller/AppController.java
+++ src/main/java/com/equipo4/nocturnemusic/controller/AppController.java
@@ -1,15 +1,27 @@
 package com.equipo4.nocturnemusic.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+
+import com.equipo4.nocturnemusic.model.*;
+
+// En lo que resuelvo HOME
+@SuppressWarnings("unused")
 
 @Controller
 public class AppController {
-
-    @GetMapping("/")
-    public String showHome() {
-        return "redirect:/products";
+    @RequestMapping("/")
+    public String landing() {
+        return "index";
     }
 
+    @RequestMapping("/home")
+	public String home(Model model, HttpSession session) {
+		
+		return "home";
+	}
 }
+
